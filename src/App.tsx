09f28623757b0a2cb74d18793263dac32e4420cd
Aathif26 <aathif0710@gmail.com>
2025-05-09 23:01:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <header className='flex items-center justify-center h-screen font-medium text-4xl'>
+      Little Lemon Website
+    </header>
+  )
+}
+
+export default App
